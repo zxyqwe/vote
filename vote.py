f8ab851a54f9776fa6ee4e7639ed057f1d8bff6c
zxyqwe <zxyqwe_2004@126.com>
2013-04-16 23:20:25 +0800

    conn ip

--- vote.py
+++ vote.py
@@ -6,9 +6,11 @@ import sys,re,os
 import gevent
 import gevent.monkey
 import urllib2
+import urllib
+import socket
 from pyquery import PyQuery
 
-PORT = ['8080','80','43','3128','43']
+ports = ['8080','80','43','3128']
 urls=[]
 gevent.monkey.patch_all()
 
@@ -19,13 +21,27 @@ def get_proxy(url):
 		for item in d("#proxylisttb table tr td"):
 			ip = item.text
 			if ip and re.match(r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})", ip):
-				print ip
+				urls.append(ip)
+		for port in ports:
+			jobs = [gevent.spawn(vote,ip,port) for ip in urls]
+		gevent.joinall(jobs)
+		r.close()
 	except Exception as e:
 		print e
 		print 'get_proxy quit!'
 
-def vote():
-	pass #do as your wish
+def vote(ip,port):
+	try:
+		r = urllib.urlopen(r'http://hi.baidu.com/zxyqwe',proxies = {'http':('http://'+ip+':'+port)}) 
+		print ip,port	
+		r.close()
+	except Exception as e:
+		print e	
+		pass	
 
 if __name__ == '__main__':
-	get_proxy("http://www.cnproxy.com/proxy1.html")
+	socket.setdefaulttimeout(5)
+	for i in range(1,3):
+		get_proxy("http://www.cnproxy.com/proxyedu"+str(i)+".html")
+	for i in range(1,11):
+		get_proxy("http://www.cnproxy.com/proxy"+str(i)+".html")
